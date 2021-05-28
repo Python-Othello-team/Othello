bbac49a7a269d0f1682e48d1224ea6aa81d200b0
dpwns <dpwns0827@naver.com>
2021-05-28 20:36:17 +0900

    .

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import random
 
-print(asd)
+#0 빈칸, 1 하양, 2 검정
+def color_check(color):
+    if color == 1 or color == 2:
+        return True
+    else:
+        return False
+
+def make_base():
+    board = []
+    return board
+
+def available_check(color, board):
+    if not color_check(color): return []
+    return
+
+def get_score(color, board):
+    if not color_check(color): return 0
+    return
